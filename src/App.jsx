c95b9f37359db50d10faa586c89e7fde2dc44eab
Camilo S. Soler Corredor <camilosoler@cbit-online.com>
2020-06-01 13:09:47 -0500

    [Cssc;#master ; feat]: Se realiza la creacion de crud react

---
+++ src/App.jsx
@@ -0,0 +1,132 @@
+import React from 'react';
+import shortid from 'shortid'
+
+
+
+function App() {
+
+
+
+  const [tarea ,setTarea]= React.useState('')
+  const [tareas ,setTareas] = React.useState ([])
+  const [modoEdicion, setModoedicion] = React.useState(false)
+  const [id ,setId] = React.useState ('')
+  const agregarTarea = e =>{
+    e.preventDefault()
+    if(!tarea.trim()){
+      console.log('Elemento vacio')
+      return
+    }
+    console.log(tarea)
+    setTareas([ 
+      ...tareas,
+      {id:shortid.generate(), nombreTarea:tarea}
+    ])
+    setTarea('')
+  }
+
+  const eliminarTarea = id => {
+        const arrayFiltrado = tareas.filter(item => item.id !== id)
+    setTareas(arrayFiltrado)  
+  }
+
+  const editar = item =>{
+    console.log(item)
+    setModoedicion(true)
+    setTarea(item.nombreTarea)
+    setId(item.id)
+  }
+
+  const editarTarea = e =>{
+    e.preventDefault()
+    if(!tarea.trim()){
+     console.log('Elemento vacio')
+     return
+    }
+
+    const arrayEditado = tareas.map
+    (item =>item.id === id ? {id:id,nombreTarea:tarea}: item )
+setTareas(arrayEditado)
+    setModoedicion(false)
+    setTarea('')
+    setId('')
+  }
+
+
+  return (
+    <div className="container">
+    <h1 className="text-center mt-5">CRUD SIMPLE</h1>
+    <hr/>
+
+    <div className="row">
+    <div className="col-8">
+       <h4 className="text-center">
+        Lista de tareas
+       </h4>
+
+       <ul className="list-group">
+       {
+
+        tareas.length === 0 ? (<li className="list-group-item">No hay tareas</li>) : (
+          
+                 tareas.map(item =>(
+
+          <li  key = {item.id} className="list-group-item">
+          <span className="lead">
+           {item.nombreTarea}    
+          </span>
+          <button className="btn btn-danger btn-sm float-right mx-2"
+          onClick= {() => eliminarTarea(item.id)}
+          >Eliminar
+
+          </button>
+          <button className="btn btn-warning btn-sm float-right"
+          onClick = {() => editar(item)}
+          >
+          Editar
+          
+          </button>
+    
+          </li> 
+         ))
+        
+        )
+       }
+      
+
+
+       </ul>
+    </div>
+    <div className="col-4">
+    <h4 className="text-center">
+        {
+          modoEdicion ? 'Editar Tarea':'Agregar tarea'
+        }
+       </h4>
+
+       <form  onSubmit={modoEdicion ?editarTarea : agregarTarea}>
+       <input 
+       type="text" 
+       className="form-cntrol mb-2"
+       placeholder= "Ingrese tarea"
+       onChange ={e => setTarea(e.target.value)}
+       value={tarea}
+       />
+       {
+         modoEdicion ? (
+          <button className="btn btn-warning btn-block" type="submit">Editar</button>
+
+         ) : (  <button className="btn btn-dark btn-block" type="submit">Agregar</button>
+         )
+       }
+       
+       </form>
+    </div>
+    </div>
+    </div>
+    
+
+  );
+}
+
+export default App;
